00678542c9f241f652337af96ad0cdce123b0ee6
me <me@me.me.com>
2020-10-23 14:55:41 -0700

    create create and show controllers with views

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -2,21 +2,42 @@ using Microsoft.AspNetCore.Mvc;
 using HairSalon.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace HairSalon.Controllers
 {
-    public class ClientsController : Controller
+  public class ClientsController : Controller
+  {
+    private readonly HairSalonContext _db;
+    public ClientsController(HairSalonContext db)
     {
-        private readonly HairSalonContext _db;
-        public ClientsController(HairSalonContext db)
-        {
-            _db = db;
-        }
-
-        public ActionResult Index()
-        {
-            List<Client> clients = _db.Clients.ToList();
-            return View(clients);
-        }
+      _db = db;
     }
+
+    public ActionResult Index()
+    {
+      List<Client> clients = _db.Clients.Include(x => x.Stylist).ToList();
+      return View(clients);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "LastName");
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Client client = _db.Clients.FirstOrDefault(x => x.ClientId == id);
+      return View(client);
+    }
+  }
 }
--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -5,18 +5,37 @@ using System.Linq;
 
 namespace HairSalon.Controllers
 {
-    public class StylistsController : Controller
+  public class StylistsController : Controller
+  {
+    private readonly HairSalonContext _db;
+    public StylistsController(HairSalonContext db)
     {
-        private readonly HairSalonContext _db;
-        public StylistsController(HairSalonContext db)
-        {
-            _db = db;
-        }
-
-        public ActionResult Index()
-        {
-            List<Stylist> stylists = _db.Stylists.ToList();
-            return View(stylists);
-        }
+      _db = db;
     }
+
+    public ActionResult Index()
+    {
+      List<Stylist> stylists = _db.Stylists.ToList();
+      return View(stylists);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Stylist stylist)
+    {
+      _db.Stylists.Add(stylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Stylist stylist = _db.Stylists.FirstOrDefault(x => x.StylistId == id);
+      stylist.Clients = _db.Clients.Where(x => x.StylistId == id).ToList();
+      return View(stylist);
+    }
+  }
 }
